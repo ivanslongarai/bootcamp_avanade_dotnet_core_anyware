bb8b5905d678389ba837b7a121e0e26b4d1f5862
Ivan Longarai <ivanslongarai@gmail.com>
2021-08-23 14:37:23 -0300

    Create Project

---
+++ ClassAndObjectsOnProjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassAndObjectsOnProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
