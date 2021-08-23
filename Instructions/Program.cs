b4e96dd79706b002a319f12b33ec32d65ba5d460
Ivan Longarai <ivanslongarai@gmail.com>
2021-08-23 07:31:49 -0300

    Create Project

---
+++ Instructions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Instructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Declarations () {
+            
+        }
+    }
+}
